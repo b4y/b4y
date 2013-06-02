b4a53efbfa7b120f5765a6dd6f48f58c86621f84
Jigang Hao <jigang_hao@hotmail.com>
2013-06-02 10:30:20 -0700

    refactor trivial

--- app/util/DbUtil.scala
+++ app/util/DbUtil.scala
@@ -6,22 +6,7 @@ import org.apache.commons.lang3.StringUtils
 import java.util.Date
 
 object DbUtil {
-  def delete(db: JacksonDBCollection[AnyRef,  String], id: String) = db.removeById(id)
   def load(db: JacksonDBCollection[AnyRef,  String], id: String) = db.findOneById(id)
-  //  def findByItemIds(itemIds: List[String]): List[ProductItem] = itemIds.map(load(_))
-
-  def isFieldValueInDb(db: JacksonDBCollection[AnyRef,  String], field: String, value: String): Boolean = {
-    val cursor = db.find().is(field, value)
-    null != cursor && cursor.hasNext
-  }
-
-  def findOneByField(db: JacksonDBCollection[AnyRef,  String], field: String, value: String): AnyRef = {
-    val cursor = db.find().is(field, value)
-    if (null != cursor && cursor.hasNext)
-      cursor.next
-    else
-      null
-  }
 
   def findAll(db: JacksonDBCollection[AnyRef,  String]): List[AnyRef] = {
     val cursor = db.find()
@@ -33,12 +18,28 @@ object DbUtil {
     dbEntries
   }
 
+  def findOneByField(db: JacksonDBCollection[AnyRef,  String], field: String, value: String): AnyRef = {
+    val cursor = db.find().is(field, value)
+    if (null != cursor && cursor.hasNext)
+      cursor.next
+    else
+      null
+  }
+
+  def isFieldValueInDb(db: JacksonDBCollection[AnyRef,  String], field: String, value: String): Boolean = {
+    val cursor = db.find().is(field, value)
+    null != cursor && cursor.hasNext
+  }
+
   def save(db: JacksonDBCollection[AnyRef,  String], entityWithId: DbId):AnyRef ={
     if (StringUtils.isBlank(entityWithId.id)){
       entityWithId.id = (new Date()).getTime.toString
     }
     db.save(entityWithId).getSavedObject
   }
+
+  def delete(db: JacksonDBCollection[AnyRef,  String], id: String) = db.removeById(id)
+
 }
 
 trait DbId{
